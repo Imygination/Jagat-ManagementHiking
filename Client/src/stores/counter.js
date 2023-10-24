191dfdb6f818f98a275ee0eb5e61a9dc0fcc2464
Gema <gemaramadhan20@gmail.com>
2023-10-24 13:26:35 +0700

    feat: Add Skeleton Vue, Store, and Route

---
+++ Client/src/stores/counter.js
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+// import axios from 'axios'
+// import Swal from 'sweetalert2'
+
+// const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'https://ch1news.gemaramadhan.online'
+
+export const useIndexStore = defineStore('index', {
+  state: () => ({}),
+  getters: {},
+  actions: {}
+})
---
+++ Client/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>HOLLAA</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
